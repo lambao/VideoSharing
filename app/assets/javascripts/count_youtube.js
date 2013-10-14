40fc027d16a19d5969db858f33856f39ace917a5
Luu Bao Lam <lamluu@cnc.com.vn>
2013-10-14 17:52:01 +0700

    Test facebook like, commend, count view on heroku

--- app/assets/javascripts/count_youtube.js
+++ app/assets/javascripts/count_youtube.js
@@ -1,9 +1,14 @@
 function onYouTubePlayerReady() {
     alert("hehi");
+    console.log("onYouTubePlayerReady");
     ytplayer = document.getElementById("ytplayer");
     ytplayer.addEventListener("onStateChange", "onytplayerStateChange");
 }
 
 function onytplayerStateChange(newState) {
     alert("Player's new state: " + newState);
+}
+
+function onYouTubeIframeAPIReady() {
+    console.log("onYouTubeIframeAPIReady");
 }
