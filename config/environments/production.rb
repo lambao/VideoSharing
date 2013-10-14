d1b656c55630165bd421d657d9680a3fcaa8f920
Luu Bao Lam <lamluu@cnc.com.vn>
2013-10-14 10:44:55 +0700

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ VideoSharing::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => 'vast-plateau-9965.herokuapp.com' }
 end
