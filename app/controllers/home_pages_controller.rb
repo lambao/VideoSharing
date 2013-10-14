f0390aa9f419053708f7e46622d82a82948ea7cb
Luu Bao Lam <lamluu@cnc.com.vn>
2013-10-14 11:02:13 +0700

    prepare for lastes videos page

--- app/controllers/home_pages_controller.rb
+++ app/controllers/home_pages_controller.rb
@@ -2,4 +2,7 @@ class HomePagesController < ApplicationController
   def home
     @lastestVideo = Video.order("created_at DESC").limit(12)
   end
+  def lastest
+    @lastestVideo = Video.order("created_at DESC").paginate(:page => params[:page], :per_page => 12)
+  end
 end
