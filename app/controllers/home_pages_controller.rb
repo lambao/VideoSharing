cb9f28a49e8acebaeb22decee38f36f4837e59c5
Luu Bao Lam <lamluu@cnc.com.vn>
2013-10-17 09:36:54 +0700

    final release

--- app/controllers/home_pages_controller.rb
+++ app/controllers/home_pages_controller.rb
@@ -1,11 +1,12 @@
 class HomePagesController < ApplicationController
-
-
   def home
     @lastestVideo = Video.order("created_at DESC").limit(8)
     @hotVideo = Video.order("view_count DESC").limit(8)
   end
   def lastest
-    @lastestVideo = Video.order("created_at DESC").paginate(:page => params[:page], :per_page => 12)
+    @lastestVideo = Video.order("created_at DESC").paginate(:page => params[:page], :per_page => 8)
+  end
+  def hot
+    @hotVideos = Video.order("view_count DESC").paginate(:page => params[:page], :per_page => 8)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ VideoSharing::Application.routes.draw do
   match 'my_video', to: 'videos', via: [:get]
   match 'increase_count', to: 'videos#increase_count_view', via: [:put]
   match 'lastest', to: 'home_pages#lastest', via: [:get]
+  match 'hot', to:'home_pages#hot', via: [:get]
   match 'user_list', to: 'manage_users#index', via: [:get]
   get 'manage_user/edit/:id', to: 'manage_users#edit'
   post 'manage_user/edit/:id', to: 'manage_users#update'
